6304510a7e70e663335ea2f58ac3afbd28f3354f
bharatchandran <bharatchandrankr@gmail.com>
2023-08-18 12:02:46 +0530

    corrected intrest and group list in urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,11 +3,10 @@ from . import views
 
 urlpatterns = [ 
     path('', views.home, name='home'),
-    path('interests/', views.Interest.as_view(), name='interest_index'),
-    path('interests/<int:pk>/', views.Interest.as_view(), name='interest_detail'),
+    path('interests/', views.InterestList.as_view(), name='interest_index'),
     path('interests/<int:pk>/update/', views.InterestUpdate.as_view(), name='interest_update'),
     path('interests/<int:pk>/delete/', views.InterestDelete.as_view(), name='interest_delete'),
-    path('interests/<int:id>/groups/', views.Groups.as_view(), name='group_index'),
+    path('interests/<int:id>/groups/', views.GroupsList.as_view(), name='group_index'),
     path('interests/<int:id>/groups/<int:pk>/', views.Groups.as_view(), name='group_detail'),
     path('interests/<int:id>/groups/<int:pk>/update/', views.GroupUpdate.as_view(), name='group_update'),
     path('interests/<int:id>/groups/<int:pk>/delete/', views.GroupDelete.as_view(), name='group_delete'),
