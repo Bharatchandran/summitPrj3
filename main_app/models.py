414f4c162bae9e25ec07d2f13b1855a234d068d3
pxner0 <napogonowski@outlook.com>
2023-08-18 14:10:42 +1000

    Base html Initialised"

--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-
-
-
-
+U
 class Interest(models.Model):
     owner = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
