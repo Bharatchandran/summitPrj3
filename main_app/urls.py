294b03eebc80cfdfbe23c74eac9ead1a99c424ea
pxner0 <napogonowski@outlook.com>
2023-08-18 16:41:39 +1000

    minor bugs fixed, confirm delete template

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ 
+urlpatterns = [
     path('', views.home, name='home'),
     path('interests/', views.InterestList.as_view(), name='interest_index'),
-    path('interests/<int:pk>/update/', views.InterestUpdate.as_view(), name='interest_update'),
-    path('interests/<int:pk>/delete/', views.InterestDelete.as_view(), name='interest_delete'),
-    path('interests/<int:id>/groups/', views.GroupsList.as_view(), name='group_index'),
-    path('interests/<int:id>/groups/<int:pk>/', views.Groups.as_view(), name='group_detail'),
-    path('interests/<int:id>/groups/<int:pk>/update/', views.GroupUpdate.as_view(), name='group_update'),
-    path('interests/<int:id>/groups/<int:pk>/delete/', views.GroupDelete.as_view(), name='group_delete'),
+    path('interests/<int:pk>/update/',
+         views.InterestUpdate.as_view(), name='interest_update'),
+    path('interests/<int:pk>/delete/',
+         views.InterestDelete.as_view(), name='interest_delete'),
+    path('interests/<int:id>/groups/',
+         views.GroupList.as_view(), name='group_index'),
+    path('interests/<int:id>/groups/<int:pk>/',
+         views.GroupDetail.as_view(), name='group_detail'),
+    path('interests/<int:id>/groups/<int:pk>/update/',
+         views.GroupUpdate.as_view(), name='group_update'),
+    path('interests/<int:id>/groups/<int:pk>/delete/',
+         views.GroupDelete.as_view(), name='group_delete'),
 ]
