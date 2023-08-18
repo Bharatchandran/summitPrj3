366654b04daabf1e25eb21cd1e3bbaa809d8514b
tobyxchan <tobyxchan@gmail.com>
2023-08-18 15:11:15 +1000

    Wrote views and urls for interests and groups

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [ 
     path('', views.home, name='home'),
-    
-
-
-
+    path('interests/', views.Interest.as_view(), name='interest_index'),
+    path('interests/<int:pk>/', views.Interest.as_view(), name='interest_detail'),
+    path('interests/<int:pk>/update/', views.InterestUpdate.as_view(), name='interest_update'),
+    path('interests/<int:pk>/delete/', views.InterestDelete.as_view(), name='interest_delete'),
+    path('groups/', views.Groups.as_view(), name='group_index'),
+    path('groups/<int:pk>/', views.Groups.as_view(), name='group_detail'),
+    path('groups/<int:pk>/update/', views.GroupUpdate.as_view(), name='group_update'),
+    path('groups/<int:pk>/delete/', views.GroupDelete.as_view(), name='group_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post, Interest, Topic, Group
+
 
 # Create your views here.
-
-
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+class InterestList(LoginRequiredMixin, ListView):
+    model = Interest
+
+class InterestDetail(LoginRequiredMixin, DetailView):
+    model = Interest
+
+class InterestCreate(LoginRequiredMixin, CreateView):
+    model = Interest
+    fields = ['name']
+
+class InterestUpdate(LoginRequiredMixin, UpdateView):
+    model = Interest
+    fields = ['name']
+
+class InterestDelete(LoginRequiredMixin, DeleteView):
+    model = Interest
+    success_url = '/interests'
+
+class GroupList(LoginRequiredMixin, ListView):
+    model = Group
+
+class GroupDetail(LoginRequiredMixin, DetailView):
+    model = Group
+
+class GroupCreate(LoginRequiredMixin, CreateView):
+    model = Group
+    fields = ['name']
+
+class GroupUpdate(LoginRequiredMixin, UpdateView):
+    model = Group
+    fields = ['name']
+
+class GroupDelete(LoginRequiredMixin, DeleteView):
+    model = Group
+    success_url = '/groups'
+
+def signup(request):
+    error_message = ''
+    form = UserCreationForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('interest_list')
+        else:
+            error_message = 'Invalid sign up - try again'
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
