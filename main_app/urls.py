57076f18fac4de774fdfcce15f92888194da9563
tobyxchan <tobyxchan@gmail.com>
2023-08-18 16:04:59 +1000

    Interest and group create

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('interests/<int:pk>/', views.Interest.as_view(), name='interest_detail'),
     path('interests/<int:pk>/update/', views.InterestUpdate.as_view(), name='interest_update'),
     path('interests/<int:pk>/delete/', views.InterestDelete.as_view(), name='interest_delete'),
-    path('groups/', views.Groups.as_view(), name='group_index'),
-    path('groups/<int:pk>/', views.Groups.as_view(), name='group_detail'),
-    path('groups/<int:pk>/update/', views.GroupUpdate.as_view(), name='group_update'),
-    path('groups/<int:pk>/delete/', views.GroupDelete.as_view(), name='group_delete'),
+    path('interests/<int:id>/groups/', views.Groups.as_view(), name='group_index'),
+    path('interests/<int:id>/groups/<int:pk>/', views.Groups.as_view(), name='group_detail'),
+    path('interests/<int:id>/groups/<int:pk>/update/', views.GroupUpdate.as_view(), name='group_update'),
+    path('interests/<int:id>/groups/<int:pk>/delete/', views.GroupDelete.as_view(), name='group_delete'),
 ]
