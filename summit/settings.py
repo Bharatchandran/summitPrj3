e744fee800039d56a98243a2bc86abdf14a48563
tobyxchan <tobyxchan@gmail.com>
2023-08-23 12:29:03 +1000

    sidebar on group detail

--- summit/settings.py
+++ summit/settings.py
@@ -95,6 +95,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'summit',
+        'USER': 'tobychantharasy',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
