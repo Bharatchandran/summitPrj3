19d8bab11bcce68bef7031ba7377e93fe5f13937
bharatchandran <bharatchandrankr@gmail.com>
2023-08-25 05:17:41 +0530

    made the code clean

--- main_app/views.py
+++ main_app/views.py
@@ -19,10 +19,8 @@ from random import randrange
 def home(request):
     return render(request, 'home.html')
 
-
 def about(request):
     return render(request, 'about.html')
-
 
 class InterestList(LoginRequiredMixin, ListView):
     model = Interest
@@ -33,39 +31,32 @@ class InterestList(LoginRequiredMixin, ListView):
         # Add extra context data
         context['user_id'] = self.request.user.id
         context['groups'] = Group.objects.filter(owner_id = self.request.user.id)
-        context['my_groups'] = my_group
-        
-        
+        context['my_groups'] = my_group        
         return context
 
 class InterestCreate(LoginRequiredMixin, CreateView):
     model = Interest
     fields = ['name', 'description']
-    # form.owner = self.request.user
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
-
 class InterestUpdate(LoginRequiredMixin, UpdateView):
     model = Interest
     fields = ['name']
-
 
 class InterestDelete(LoginRequiredMixin, DeleteView):
     model = Interest
     success_url = '/interests'
 
-
+@login_required
 def group_list(request, interest_id):
     groups = Group.objects.filter(interest=interest_id)
     interests = Interest.objects.all()
     currentInterest = Interest.objects.get(id=interest_id)
     user_id = request.user.id
     members = Member.objects.all().filter(user_id=user_id)
-    # members = Group.objects.member_set.all().filter(user_id=user_id)
-    print(members)
     return render(request, 'main_app/group_list.html', {
         'interest_id': interest_id,
         'groups': groups,
@@ -75,7 +66,7 @@ def group_list(request, interest_id):
         'members': members,
     })
 
-
+@login_required
 def group_detail(request, group_id):
     group = Group.objects.get(id=group_id)
     topic_form = TopicForm()
@@ -84,7 +75,6 @@ def group_detail(request, group_id):
     user_id = request.user.id
 
     def user_like_post(self):
-        print(self.user.id)
         return self.like_set.get(user_id = self.user.id)
 
     return render(request, 'main_app/group_detail.html', {
@@ -95,21 +85,18 @@ def group_detail(request, group_id):
         'user_id': user_id
     })
 
-
+@login_required
 def group_new(request, interest_id):
     group_form = GroupForm()
-
     return render(request, 'main_app/group_new.html', {
         'group_form': group_form,
         'interest_id': interest_id
     })
 
-
+@login_required
 def group_create(request, interest_id):
     random_image = ["icon.svg","initials.svg","laurent.svg","laurent-round.svg","minidenticons.svg"]
-    
     form = GroupForm(request.POST)
-
     if form.is_valid():
         new_group = form.save(commit=False)
         new_group.interest_id = interest_id
@@ -133,13 +120,10 @@ def group_create(request, interest_id):
                 print(e)
         if not new_group.image_url:
             new_group.image_url = random_image[randrange(5)]
-
-
         new_group.save()
-        
     return redirect('group_list', interest_id=interest_id)
 
-    
+@login_required
 def mygroup_index(request):
     member = Member.objects.all().filter(user_id=request.user.id)
     return render(request,'main_app/mygroup.html',{
@@ -149,10 +133,8 @@ class GroupUpdate(LoginRequiredMixin, UpdateView):
     model = Group
     fields = ['name']
 
-
 class GroupDelete(LoginRequiredMixin, DeleteView):
     model = Group
-    # success_url = '/interests'
     def get_success_url(self):
         group = self.get_object()
         interest_id = group.interest_id
@@ -160,7 +142,7 @@ class GroupDelete(LoginRequiredMixin, DeleteView):
                                    'interest_id': interest_id})
         return success_url
 
-
+@login_required
 def topic_create(request, group_id):
     form = TopicForm(request.POST)
     if form.is_valid():
@@ -170,16 +152,12 @@ def topic_create(request, group_id):
         new_topic.save()
     return redirect('group_detail', group_id=group_id)
 
-
 class TopicUpdate(LoginRequiredMixin, UpdateView):
     model = Topic
     fields = ['name']
 
-
 class TopicDelete(LoginRequiredMixin, DeleteView):
     model = Topic
-    # success_url = 'group_detail'
-    # success_url = '/interests'
     group_id = model.group_id
 
     def get_success_url(self):
@@ -189,37 +167,16 @@ class TopicDelete(LoginRequiredMixin, DeleteView):
                                    'group_id': group_id})
         return success_url
 
-# def topic_detail(request, topic_id):
-#     group = Group.objects.get()
-#     topic = Topic.objects.get(id=topic_id)
-#     topic_form = TopicForm()
-#     post_form = PostForm()
-#     topics = group.topic_set.all()
-
-#     def user_like_post(self):
-#         print(self.user.id)
-#         return self.like_set.get(user_id = self.user.id)
-
-#     return render(request, 'main_app/topic_detail.html', {
-#         'group': group,
-#         'topic_form': topic_form,
-#         'topics': topics,
-#         'post_form': post_form,
-#         'topic': topic,
-#     })
-
+@login_required
 def topic_detail(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     topic_form = TopicForm()
     post_form = PostForm()
     group = topic.group
     user_id = request.user.id
-
-    
     likes = Like.objects.all().filter(user_id = user_id)
     
     def user_like_post(self):
-        print(self.user.id)
         return self.like_set.get(user_id = self.user.id)
 
     return render(request, 'main_app/topic_detail.html', {
@@ -231,13 +188,13 @@ def topic_detail(request, topic_id):
         'likes': likes
     })
 
+@login_required
 def post_create(request, group_id, topic_id):
     form = PostForm(request.POST)
     if form.is_valid():
         new_post = form.save(commit=False)
         new_post.topic_id = topic_id
         new_post.user_id = request.user.id
-        # new_post.user_id = request.user.id
         photo_file = request.FILES.get('photo-file', None)
         if photo_file:
             s3 = boto3.client('s3')
@@ -271,7 +228,6 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
                                    'topic_id': topic_id})
         return success_url
 
-
 class PostDelete(LoginRequiredMixin, DeleteView):
     model = Post
 
@@ -283,20 +239,19 @@ class PostDelete(LoginRequiredMixin, DeleteView):
                                    'topic_id': topic_id})
         return success_url
 
-
+@login_required
 def add_member_to_group(request,interest_id,group_id):
     Member.objects.create(user_id=request.user.id, group_id=group_id, interest_id=interest_id)
     return redirect('group_detail', group_id=group_id)
 
+@login_required
 def create_like(request, group_id, post_id):
     post = Post.objects.get(id=post_id)
     topic_id = post.topic_id
     like = post.like_set.filter(user=request.user)
-    print(like)
     if len(like) == 0:
         Like.objects.create(post_id=post_id, user_id= request.user.id )
     return redirect('topic_detail', topic_id=topic_id)
-
 
 def signup(request):
     error_message = ''
