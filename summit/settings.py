789567978e8b2312d57e712184d4f275d2c6d317
tobyxchan <tobyxchan@gmail.com>
2023-08-24 17:14:58 +1000

    heroku final

--- summit/settings.py
+++ summit/settings.py
@@ -80,27 +80,27 @@ WSGI_APPLICATION = 'summit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '<DATABASE>',
-        'USER': '<USER>',
-        'PASSWORD': '<PASSWORD>',
-        'HOST': '<HOST>',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'summit',
-#         # 'USER': 'tobychantharasy',
-#         # 'PASSWORD': 'password',
-#         # 'HOST': 'localhost',
-#         # 'PORT': '5432',
+#         'NAME': '<DATABASE>',
+#         'USER': '<USER>',
+#         'PASSWORD': '<PASSWORD>',
+#         'HOST': '<HOST>',
+#         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'summit',
+        'USER': 'tobychantharasy',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
